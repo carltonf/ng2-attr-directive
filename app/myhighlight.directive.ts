d6ce8020020abaf7c7531374ca3966b11fd3b584
Carl Xiong <xiongc05@gmail.com>
2016-01-04 17:16:04 +0800

    feat(fallback): fall back to *valid* default
    * "smart" way of using JS getter and setter ;P to have modal to view mapping.
    
    * every input has its own variable, a much cleaner approach.

--- app/myhighlight.directive.ts
+++ app/myhighlight.directive.ts
@@ -6,28 +6,31 @@ import {Directive, ElementRef, Renderer, Input, OnInit} from 'angular2/core';
     '(mouseenter)': 'onMouseEnter()',
     '(mouseleave)': 'onMouseLeave()'
   },
-  inputs: ['highlightColor:myHighlight']
+  inputs: ['_setHLColor:myHighlight', '_defaultHLColor:defaultColor']
 })
-export class MyHighlightDirective implements OnInit {
-  public highlightColor:string;
-
-  // another way to have a default value, note an internal variable
-  // `_defaultHLColor` is needed.
-  private _defaultHLColor:string = 'yellow';
-  @Input() set defaultColor(colorName:string) {
-    this._defaultHLColor = colorName || this._defaultHLColor;
+export class MyHighlightDirective{
+  private _setHLColor:string;
+  private _defaultHLColor:string;
+  private FALLBACK_COLOR = 'yellow';
+  private SUPPORTED_COLORS = ['green', 'yellow', 'cyan', 'steelblue'];
+  // `highlightColor` is the color for highlighting, i.e. the data behind the
+  // view.
+  public get _highlightColor(){
+    return (this.SUPPORTED_COLORS.indexOf(this._setHLColor) > -1)
+      ? this._setHLColor
+      : (this.SUPPORTED_COLORS.indexOf(this._defaultHLColor) > -1)
+      ? this._defaultHLColor
+      : this.FALLBACK_COLOR;
   }
+
   constructor(private el: ElementRef, private renderer: Renderer) {
   }
-  ngOnInit(){
-    // NOTE: have to be here after defaultColor has been set.
-    this.highlightColor = this.highlightColor || this._defaultHLColor;
-  }
+
   private _highlight(color: string) {
     this.renderer.setElementStyle(this.el, 'backgroundColor', color);
   }
   onMouseEnter() {
-    this._highlight(this.highlightColor);
+    this._highlight(this._highlightColor);
   }
 
   onMouseLeave() {
